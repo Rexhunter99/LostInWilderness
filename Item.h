ee24955785019229f6f21b85a068ab1f27416251
Rexhunter99 <rexhunter99@gmail.com>
2015-05-08 18:01:25 +1000

    Tweaked the data members of Item and ItemInfo to better utilize memory

--- Item.h
+++ Item.h
@@ -1,17 +1,35 @@
 
 #pragma once
 
+#include "ItemInfo.h"
 #include <string>
 #include <cstdint>
 
+/** @class Item
+ ** @brief World instance of the Item class.
+ ** The world may have many items in it of one type, this class handles that abstraction.
+ **/
 class Item
 {
 protected:
 
 public:
 
-	std::string			name;
-	std::wstring		locale_name;
-	uint16_t			id;
+  /** @var info
+   ** @brief Reference to ItemInfo instance.
+   ** Reference to the item information and code that this instance of item draws from.
+   **/
+	ItemInfo        &info;
+
+	/** @var name
+   ** @brief Unicode string for this item instance
+   ** The individual name of this particular item in the world space. This is unique to this instance.
+   **/
+	std::wstring		name;
+
+  /** @fn Item( ItemInfo &info )
+   ** The constructor that creates this item */
+	Item( ItemInfo &info ) : info( info )
+	{}
 
 };
--- ItemInfo.h
+++ ItemInfo.h
@@ -1,13 +1,29 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <stdint.h>
 
 
 class ItemInfo
 {
 public:
 
-	std::string		name;
-	std::string		locale_name;
-	uint32_t		ID;
+  /** @var name
+   ** The internal name of the item for use in string identification as opposed to using the numerical ID */
+	std::string		        name;
+	/** @var locale_name
+   ** The name of the item in the locale that the player has chosen to play in. (Language) */
+	std::wstring	        locale_name;
+	/** @var id
+	 ** The unique numerical ID of the item for use in identifiying it numerically or with data constraints */
+	uint32_t		          id;
+	/** @var data
+	 ** A special area of data for use by the item author for whatever they please. */
+  std::vector<uint8_t>  data;
+
+  ItemInfo( std::string name, std::string locale_name, uint16_t id ) : name( name ), locale_name( locale_name ), id( id )
+  {}
+
+  ~ItemInfo();
 };
