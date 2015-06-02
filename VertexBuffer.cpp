df0d6196c3061c740737b6bea5c69518d6472c38
Rexhunter99 <rexhunter99@gmail.com>
2015-06-02 23:10:38 +1000

    VertexBuffer class base implemention
    The base implementation of the VertexBuffer class that allows switching
    between legacy arrays and modern VBOs

--- VertexBuffer.cpp
+++ VertexBuffer.cpp
@@ -1,16 +1,69 @@
 
 #include "VertexBuffer.h"
+#include "Renderer.h"
 
 #include <GL/glew.h>
 
 
-VertexBuffer::VertexBuffer()
+template <class T>
+VertexBuffer<T>::VertexBuffer()
 {
-
+	if ( !GLEW_VERSION_3_0 && !GLEW_ARB_vertex_buffer_object )
+	{
+		glEnableClientState( GL_VERTEX_ARRAY );
+		glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+		glEnableClientState( GL_COLOR_ARRAY );
+		glEnableClientState( GL_NORMAL_ARRAY );
+	}
+	else
+	{
+		glGenBuffers( 1, &this->m_vbo );
+	}
 }
 
-VertexBuffer::~VertexBuffer()
+template <class T>
+VertexBuffer<T>::~VertexBuffer()
 {
-
+	if ( !GLEW_VERSION_3_0 && !GLEW_ARB_vertex_buffer_object )
+	{
+	}
+	else
+	{
+		glDeleteBuffers( 1, &this->m_vbo );
+	}
 }
 
+template <class T>
+void VertexBuffer<T>::render()
+{
+	Shader *shader = Renderer::default_shader;
+
+	if ( !GLEW_VERSION_3_0 && !GLEW_ARB_vertex_buffer_object )
+	{
+		// -- We need to use the old Vertex Array method
+		glVertexPointer( 4, GL_FLOAT, sizeof( T ), &this->m_vertices[0] );
+		glNormalPointer( GL_FLOAT, sizeof( T ), (float*)&this->m_vertices[0] + 4 );
+		glTexCoordPointer( 2, GL_FLOAT, sizeof( T ), (float*)&this->m_vertices[0] + 7 );
+		glColorPointer( 4, GL_UNSIGNED_BYTE, sizeof( T ), (float*)&this->m_vertices[0] + 9 );
+
+		glDrawArrays( GL_TRIANGLES, 0, this->m_vertices.size() );
+	}
+	else
+	{
+		// -- We can use the modern Vertex Buffer Object method
+		glBindBuffer( GL_ARRAY_BUFFER, this->m_vbo );
+
+		// -- Send the buffer data to the GPU VRAM
+		glBufferData(GL_ARRAY_BUFFER, this->m_vertices.size() * sizeof(T), &this->m_vertices[0], GL_DYNAMIC_DRAW);
+
+		glVertexAttribPointer( shader->getAttrib( "v_position" ), 4, GL_FLOAT, GL_FALSE, sizeof(T), 0 );
+		glVertexAttribPointer( shader->getAttrib( "v_normal" ), 3, GL_FLOAT, GL_FALSE, sizeof(T), (void*) (sizeof(float) * 4) );
+		glVertexAttribPointer( shader->getAttrib( "v_texcoord" ), 2, GL_FLOAT, GL_FALSE, sizeof(T), (void*) (sizeof(float) * 7) );
+		glVertexAttribPointer( shader->getAttrib( "v_diffuse" ), 4, GL_UNSIGNED_BYTE, GL_FALSE, sizeof(T), (void*) (sizeof(float) * 9) );
+
+		glDrawArrays( GL_TRIANGLES, 0, this->m_vertices.size() );
+
+		// -- Unnecessary and likely causes a slowdown if there are a lot of VertexBuffer instances running
+		glBindBuffer( GL_ARRAY_BUFFER, 0 );
+	}
+}
--- VertexBuffer.h
+++ VertexBuffer.h
@@ -3,15 +3,17 @@
 #ifndef __VERTEXBUFFER_H__
 #define __VERTEXBUFFER_H__
 
+#include "Vertex.h"
 #include <vector>
 
-class Vertex;
+
 class Renderer;
 
 
 /** @class VertexBuffer
  **	@note VertexBuffer is a wrapper for Vertex Arrays and Vertex Buffer Objects to simplify feature support of the two.
  */
+template <class T = Vertex>
 class VertexBuffer
 {
 public:
@@ -19,28 +21,36 @@ public:
 	VertexBuffer();
 	~VertexBuffer();
 
-	/** @fn add( vector<Vertex*> & vertices )
-	 ** @param vertices An STL vector of Vertex pointers
-	 ** Adds the contents of the Vertex vector to the internal Vertex Buffer
+	/** @fn add( vector<T*> & vertices )
+	 ** @param vertices An STL vector of T pointers
+	 ** Adds the contents of the T vector to the internal Vertex Buffer
 	 */
-	void add( std::vector<Vertex*> &vertices );
+	void add( std::vector<T*> &vertices );
 
 	/** @fn add( first, last )
 	 ** @param first An iterator for the first element to add to the vertex buffer
 	 ** @param last An iterator that is the very last element to add to the vertex buffer
 	 ** Adds the contents of the Vertex vector to the internal Vertex Buffer
 	 */
-	//void add( std::vector<Vertex*>::iterator first, std::vector<Vertex*>::iterator last );
+	//void add( std::vector<T*>::iterator first, std::vector<T*>::iterator last );
 
 	/** @fn clear()
 	 ** Clear the buffer to empty status, freeing all used memory.
 	 */
 	void clear();
 
+	/** @fn render()
+	 ** Upload the vertex buffer data to the Video RAM and render the buffer
+	 */
+	void render();
+
+	void addBlock( float x1, float y1, float z1, float x2, float y2, float z2, int flags );
+	void addCylinder( float x, float y, float z, float w, float l, float h, int segments );
+
 private:
 
 	unsigned int			m_vbo;
-	std::vector<Vertex*>	m_vertices;
+	std::vector<T*>			m_vertices;
 };
 
 #endif // __VERTEXBUFFER_H__
