dce4e1c8c51df112a07e31d721f22cf0fff87ab3
Razzy <razzylaqui@gmail.com>
2015-08-08 01:56:44 -0800

    Add ico, move xcf files to own folder

--- bin/LostInWilderness.sh
+++ bin/LostInWilderness.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if(bash uname -p == x86_64)
+{
+# Run the 64-Bit ELF executable
+	echo "64-Bit Architecture" 
+	./x64/liw
+}
+else
+{
 # Run the 32-Bit ELF executable
-./x86/liw
+	echo "32-Bit Architecture"
+	./x86/liw
+}
